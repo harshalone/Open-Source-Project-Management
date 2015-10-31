60371e2f641682a704ce5064a20b86e46382ac29
Harshal Lonare <OmSai@192.168.1.104>
2015-10-31 22:22:23 +0000

    sync to local and master
    sync to local and master

--- application/views/user/create-model-box.php
+++ application/views/user/create-model-box.php
@@ -4,11 +4,12 @@
                 <div class="modal-header bgm-blue">
                     <h4 class="modal-title" style="color:#fff;">Create Issue</h4>
                 </div>
+                <div id="responce">sdfds</div>
                 <div class="modal-body">
-                    <form role="form" method="post" action="/dashboard/create_issue">
+                    <form class="ajaxform" role="form" method="post" action="<?php echo site_url('dashboard/create_issue');?>">
                        <div class="row">
                             <div class="col-xs-3">
-                                <h4 class="pull-right">Project*</h4>
+                                <h4 class="pull-right">Project</h4>
                             </div>
                             <div class="col-xs-9">
                                 <select name="category" class="form-control">
@@ -34,122 +35,43 @@
                             </div>
                          </div>
                         
+                        
                         <div class="row">
+                            <div class="col-xs-3">
+                                <h4 class="pull-right">Issue type</h4>
+                            </div>
+                            <div class="col-xs-9">
+                                <select name="issuetype" class="form-control">
+                                        <option>Select one</option>
+                                        <option value="100">
+                                            Bug</option>
+                                        <option value="101">
+                                            Story</option>
+                                        <option value="102">
+                                            Technical Story</option> 
+                                    </select>
+                            </div>
+                         </div>
+                         <div class="row">
                             <div class="col-xs-3">
                                 <h4 class="pull-right">Title</h4>
                             </div>
                             <div class="col-xs-9">
                                 <input type="text" name="title" value="" class="input-lg form-control fg-input" id="exampleInputEmail1" placeholder="Enter Title">
                             </div>
-                         </div>
+                         </div> 
+                        
                         <div class="row">
                             <div class="col-xs-3">
-                                <h4 class="pull-right">Issue type</h4>
+                                <h4 class="pull-right">Description</h4>
                             </div>
                             <div class="col-xs-9">
-                                <select name="category" class="form-control">
-                                        <option>Category</option>
-                                        <option value="Web development">
-                                            Web development</option>
-                                        <option value="Web Design">
-                                            Web Design</option>
-                                        <option value="Writing &amp; Translation">
-                                            Writing &amp; Translation</option>
-                                        <option value="Sales &amp; Marketing">
-                                            Sales &amp; Marketing</option>
-                                        <option value="Admin Support">
-                                            Admin Support</option>
-                                        <option value="Data Entry">
-                                            Data Entry</option>
-                                        <option value="Programming">
-                                            Programming</option>
-                                        <option value="Other">
-                                            Other</option>
-
-                                    </select>
+                                <textarea name="description" class="form-control" style="resize: vertical;" rows="10" placeholder="Please type some project summary here..."></textarea>
                             </div>
-                         </div>
-                                
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <div class="fg-line form-group">
-                                    <select name="category" class="form-control">
-                                        <option>Category</option>
-                                        <option value="Web development">
-                                            Web development</option>
-                                        <option value="Web Design">
-                                            Web Design</option>
-                                        <option value="Writing &amp; Translation">
-                                            Writing &amp; Translation</option>
-                                        <option value="Sales &amp; Marketing">
-                                            Sales &amp; Marketing</option>
-                                        <option value="Admin Support">
-                                            Admin Support</option>
-                                        <option value="Data Entry">
-                                            Data Entry</option>
-                                        <option value="Programming">
-                                            Programming</option>
-                                        <option value="Other">
-                                            Other</option>
-
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-xs-3">
-                                <div class="fg-line form-group">
-
-                                    <select name="location" class="form-control" data-placeholder="Choose a Country...">
-                                        <option>Location</option>
-                                        <option value="AF">Afghanistan</option>
-                                        <option value="AX">Åland</option>
-                                        <option value="AL">Albania</option>
-                                        <option value="DZ">Algeria</option>
-                                        <option value="AS">American Samoa</option>
-                                        <option value="AD">Andorra</option>
-                                        <option value="AO">Angola</option>
-                                        <option value="AI">Anguilla</option>
-                                        <option value="AQ">Antarctica</option>
-                                        <option value="AG">Antigua and Barbuda</option>
-                                        <option value="AR">Argentina</option> 
-                                    </select>
-
-                                </div>
-                            </div>
-                            <div class="col-xs-3">
-                                <div class="fg-line form-group">
-                                    <select name="budget" class="form-control">
-                                        <option>Budget</option>
-                                        <option value="199">199</option>
-                                        <option value="299">299</option>
-                                        <option value="599">599</option>
-                                        <option value="999">999</option>
-                                        <option value="1599">1599</option>
-                                        <option value="1999">1999</option>
-                                        <option value="ongoing">ongoing</option>
-
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-xs-3">
-                                <div class="fg-line form-group">
-                                    <input type="text" value="" name="skills" class="input-lg form-control fg-input" placeholder="ex. wordpress, php, drupal">
-                                </div>
-                            </div>
-
-
-
-                            <div class="col-xs-12">
-                                <div class="fg-line form-group">
-                                    <div class="form-group">
-                                        <div class="fg-line">
-                                            <textarea name="description" class="form-control" style="resize: vertical;" rows="10" placeholder="Please type some project summary here..."></textarea>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                         </div>  
                          
                         <div class="clearfix"></div>
-                   </div>  
+                   
                 </div>
                 <div class="modal-footer">
                     <button type="submit" type="button" class="btn btn-link">Save changes</button>
